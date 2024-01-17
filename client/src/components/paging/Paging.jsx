ff403566f514fe4723e8afac96d30050aa8f6274
Matias <SaliskyMatias@gmail.com>
2024-01-17 02:47:14 -0300

    Mejorando el paginado

--- client/src/components/paging/Paging.jsx
+++ client/src/components/paging/Paging.jsx
@@ -4,11 +4,22 @@ import { useSelector } from 'react-redux';
 
 const Paging = ({ handlePagination }) => {
     const currentPage = useSelector((state) => state.currentPage);
+    const allvideogames = useSelector((state) => state.videogamesBackUp);
+    const videogamesFiltered = useSelector((state) => state.videogamesFiltered);
+    const filters = useSelector((state) => state.filters);
+
+    let totalPages;
+
+    if(filters) {
+        totalPages = Math.ceil(videogamesFiltered.length / 15);
+    } else {
+        totalPages = Math.ceil(allvideogames.length / 15);
+    }
 
     return (
         <div className={style.paging_container}>
             <button name="prev" onClick={(event) => handlePagination(event)}>Prev</button>
-            <p>{currentPage + 1}</p>
+            <p>{currentPage + 1} de {totalPages}</p>
             <button name="next" onClick={(event) => handlePagination(event)}>Next</button>
         </div>
     );
